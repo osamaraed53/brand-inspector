c95e66800894895f67864750c4c0f3e6fe077a28
Osama Raed <osamaraed53@gmail.com>
2026-03-26 10:45:51 +0300

    FE - create Constants folder and create Error messages constant class

---
+++ src/Frontend/BrandInspector/Constants/ErrorMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace BrandInspector.Constants
+{
+    public  static class ErrorMessages
+    {
+        public const string InvalidFile = "Invalid File.";        
+        public const string InvalidCredentials = " Invalid credentials";
+    }
+}
