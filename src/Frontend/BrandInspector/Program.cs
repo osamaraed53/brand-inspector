9cdf2127a958272fb1dafcf503645cf2463963aa
Osama Raed <osamaraed53@gmail.com>
2026-03-26 08:43:42 +0300

    FE - Delete form 1 and update the program.cs to change the start form

--- src/Frontend/BrandInspector/Program.cs
+++ src/Frontend/BrandInspector/Program.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BrandInspector.Views;
+using System;
 using System.Windows.Forms;
 
 namespace BrandInspector
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
