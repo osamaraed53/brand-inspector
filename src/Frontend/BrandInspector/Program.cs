b979d4e85c671c8f507d820531a365861ef696e4
Osama Raed <osamaraed53@gmail.com>
2026-03-27 22:11:28 +0300

    Change navigation behaviour in the project - add navigation service - add services to  service collection - add service collection from DependencyInjection to program

--- src/Frontend/BrandInspector/Program.cs
+++ src/Frontend/BrandInspector/Program.cs
@@ -1,4 +1,8 @@
-﻿using BrandInspector.Views;
+﻿using BrandInspector.Presenters;
+using BrandInspector.Services.Interfaces;
+using BrandInspector.Services;
+using BrandInspector.Views;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Windows.Forms;
 
@@ -6,13 +10,35 @@ namespace BrandInspector
 {
     internal static class Program
     {
+        public static ServiceProvider ServiceProvider { get; private set; }
 
         [STAThread]
         static void Main()
         {
+
+            var services = new ServiceCollection();
+
+            // Services
+            services.AddSingleton<INavigationService, NavigationService>();
+
+            // Presenters
+            services.AddTransient<LoginPresenter>();
+            services.AddTransient<MainPresenter>();
+
+            // Forms
+            services.AddTransient<LoginForm>();
+            services.AddTransient<MainForm>();
+
+            // Register services
+            services.AddTransient<MainForm>();
+
+            ServiceProvider = services.BuildServiceProvider();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            
+            var loginForm = ServiceProvider.GetRequiredService<LoginForm>();
+            Application.Run(loginForm);
         }
     }
 }
---
+++ src/Frontend/BrandInspector/Services/Interfaces/INavigationService.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace BrandInspector.Services.Interfaces
+{
+    public interface INavigationService
+    {
+        void Show<TForm>(Form current) where TForm : Form;
+        void ShowDialog<TForm>() where TForm : Form;
+
+
+    }
+}
