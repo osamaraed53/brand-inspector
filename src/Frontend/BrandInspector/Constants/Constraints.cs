bb7966bc8f148f57a84a9c6308252a3521fb1ac6
Osama Raed <osamaraed53@gmail.com>
2026-03-28 23:28:09 +0300

    update constants - add Constaraints file - add and update  new values for error messages

---
+++ src/Frontend/BrandInspector/Constants/Constraints.cs
@@ -0,0 +1,8 @@
+﻿namespace BrandInspector.Constants
+{
+    public  static class Constraints
+    {
+        public const double SizeTolerance = 5.0;
+        public const int LenOfSampleText = 30;
+    }
+}
--- src/Frontend/BrandInspector/Constants/ErrorMessages.cs
+++ src/Frontend/BrandInspector/Constants/ErrorMessages.cs
@@ -3,6 +3,8 @@
     public  static class ErrorMessages
     {
         public const string InvalidFile = "Invalid File.";        
-        public const string InvalidCredentials = " Invalid credentials";
+        public const string InvalidCredentials = "Invalid credentials.";
+        public const string InvalidFileType = "Only .pptx files are allowed.";
+
     }
 }
