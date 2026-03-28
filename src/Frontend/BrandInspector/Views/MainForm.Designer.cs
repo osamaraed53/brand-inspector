4636f47c0d2795831eb79432f470a2da696e30fa
Osama Raed <osamaraed53@gmail.com>
2026-03-28 05:20:50 +0300

    add data grid view on main area with style

--- src/Frontend/BrandInspector/Views/MainForm.Designer.cs
+++ src/Frontend/BrandInspector/Views/MainForm.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
@@ -39,13 +41,15 @@
             this.colorsBtn = new System.Windows.Forms.Button();
             this.sizeBtn = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.mainAreaPanel = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
+            this.errorsDataGridView = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
-            this.panel4.SuspendLayout();
+            this.mainAreaPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorsDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -55,7 +59,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1154, 83);
+            this.panel1.Size = new System.Drawing.Size(1164, 83);
             this.panel1.TabIndex = 0;
             // 
             // tableLayoutPanel1
@@ -70,7 +74,7 @@
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1154, 83);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1164, 83);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // tableLayoutPanel2
@@ -87,7 +91,7 @@
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(1148, 35);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1158, 35);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // browseBtn
@@ -96,10 +100,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.browseBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.browseBtn.Location = new System.Drawing.Point(968, 5);
+            this.browseBtn.Location = new System.Drawing.Point(976, 5);
             this.browseBtn.Margin = new System.Windows.Forms.Padding(5);
             this.browseBtn.Name = "browseBtn";
-            this.browseBtn.Size = new System.Drawing.Size(175, 25);
+            this.browseBtn.Size = new System.Drawing.Size(177, 25);
             this.browseBtn.TabIndex = 1;
             this.browseBtn.Text = "Browse...";
             this.browseBtn.UseVisualStyleBackColor = true;
@@ -124,7 +128,7 @@
             this.textBox1.MinimumSize = new System.Drawing.Size(900, 20);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(900, 20);
+            this.textBox1.Size = new System.Drawing.Size(904, 20);
             this.textBox1.TabIndex = 4;
             // 
             // tableLayoutPanel3
@@ -143,7 +147,7 @@
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(1148, 36);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1158, 36);
             this.tableLayoutPanel3.TabIndex = 1;
             // 
             // fontsBtn
@@ -152,7 +156,7 @@
             this.fontsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.fontsBtn.Location = new System.Drawing.Point(60, 3);
             this.fontsBtn.Name = "fontsBtn";
-            this.fontsBtn.Size = new System.Drawing.Size(160, 30);
+            this.fontsBtn.Size = new System.Drawing.Size(162, 30);
             this.fontsBtn.TabIndex = 0;
             this.fontsBtn.Text = "Scan Fonts";
             this.fontsBtn.UseVisualStyleBackColor = true;
@@ -161,9 +165,9 @@
             // 
             this.colorsBtn.Dock = System.Windows.Forms.DockStyle.Fill;
             this.colorsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.colorsBtn.Location = new System.Drawing.Point(226, 3);
+            this.colorsBtn.Location = new System.Drawing.Point(228, 3);
             this.colorsBtn.Name = "colorsBtn";
-            this.colorsBtn.Size = new System.Drawing.Size(160, 30);
+            this.colorsBtn.Size = new System.Drawing.Size(162, 30);
             this.colorsBtn.TabIndex = 1;
             this.colorsBtn.Text = "Scan Colors";
             this.colorsBtn.UseVisualStyleBackColor = true;
@@ -172,9 +176,9 @@
             // 
             this.sizeBtn.Dock = System.Windows.Forms.DockStyle.Fill;
             this.sizeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sizeBtn.Location = new System.Drawing.Point(392, 3);
+            this.sizeBtn.Location = new System.Drawing.Point(396, 3);
             this.sizeBtn.Name = "sizeBtn";
-            this.sizeBtn.Size = new System.Drawing.Size(160, 30);
+            this.sizeBtn.Size = new System.Drawing.Size(162, 30);
             this.sizeBtn.TabIndex = 2;
             this.sizeBtn.Text = "Scan Sizes";
             this.sizeBtn.UseVisualStyleBackColor = true;
@@ -185,18 +189,19 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel3.Location = new System.Drawing.Point(0, 499);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1154, 62);
+            this.panel3.Size = new System.Drawing.Size(1164, 62);
             this.panel3.TabIndex = 2;
             // 
-            // panel4
+            // mainAreaPanel
             // 
-            this.panel4.BackColor = System.Drawing.SystemColors.Highlight;
-            this.panel4.Controls.Add(this.panel5);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel4.Location = new System.Drawing.Point(0, 83);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1154, 416);
-            this.panel4.TabIndex = 3;
+            this.mainAreaPanel.BackColor = System.Drawing.SystemColors.Highlight;
+            this.mainAreaPanel.Controls.Add(this.errorsDataGridView);
+            this.mainAreaPanel.Controls.Add(this.panel5);
+            this.mainAreaPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainAreaPanel.Location = new System.Drawing.Point(0, 83);
+            this.mainAreaPanel.Name = "mainAreaPanel";
+            this.mainAreaPanel.Size = new System.Drawing.Size(1164, 416);
+            this.mainAreaPanel.TabIndex = 3;
             // 
             // panel5
             // 
@@ -207,15 +212,50 @@
             this.panel5.Size = new System.Drawing.Size(200, 416);
             this.panel5.TabIndex = 0;
             // 
+            // errorsDataGridView
+            // 
+            this.errorsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.errorsDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.errorsDataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.errorsDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.errorsDataGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.errorsDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.errorsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.errorsDataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.errorsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.errorsDataGridView.EnableHeadersVisualStyles = false;
+            this.errorsDataGridView.GridColor = System.Drawing.Color.LightGray;
+            this.errorsDataGridView.Location = new System.Drawing.Point(200, 0);
+            this.errorsDataGridView.Name = "errorsDataGridView";
+            this.errorsDataGridView.ReadOnly = true;
+            this.errorsDataGridView.RowHeadersVisible = false;
+            this.errorsDataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.errorsDataGridView.Size = new System.Drawing.Size(964, 416);
+            this.errorsDataGridView.TabIndex = 1;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1154, 561);
-            this.Controls.Add(this.panel4);
+            this.ClientSize = new System.Drawing.Size(1164, 561);
+            this.Controls.Add(this.mainAreaPanel);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
-            this.MinimumSize = new System.Drawing.Size(1160, 600);
+            this.MinimumSize = new System.Drawing.Size(1180, 600);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -225,7 +265,8 @@
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
             this.tableLayoutPanel3.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
+            this.mainAreaPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorsDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -234,7 +275,7 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel mainAreaPanel;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
@@ -245,5 +286,6 @@
         private System.Windows.Forms.Button colorsBtn;
         private System.Windows.Forms.Button sizeBtn;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView errorsDataGridView;
     }
 }
