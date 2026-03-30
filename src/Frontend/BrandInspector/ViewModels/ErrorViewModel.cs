4bc5f3b3f53e8d1cbae898ea4e50aa00e1fcfed7
Osama Raed <osamaraed53@gmail.com>
2026-03-30 22:27:40 +0300

    Create Error view model to use it in data grid view

---
+++ src/Frontend/BrandInspector/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace BrandInspector.ViewModels
+{
+    public class ErrorViewModel
+    {
+        [DisplayName("Slide #")]
+        public int SlideNumber { get; set; }
+        [DisplayName("Shape Type")]
+        public string ShapeType { get; set; }
+        [DisplayName("Sample Text")]
+        public string SampleText { get; set; }
+        [DisplayName("Font  Family")]
+
+        public string FontFamily { get; set; }
+        [DisplayName("Font Size")]
+        public double FontSizePt { get; set; }
+        [DisplayName("Text Color")]
+
+        public string ColorHex { get; set; }
+        [DisplayName("Compliance")]
+        public string Compliance { get; set; }
+
+        //Slide #, Shape Type, Sample Text (first 30 chars), Font  Family, Font Size(pt), Text Color(#RRGGBB), Compliance (Pass/Fail + reason).
+    }
+}
