551b12130065b944fd19421abbd2928e787aeb4d
Osama Raed <osamaraed53@gmail.com>
2026-03-30 19:34:13 +0300

    Create constants folde and create error messages file with messages

---
+++ src/Backend/BrandInspector/Constants/ErrorMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace BrandInspector.Constants;
+
+public static class ErrorMessages
+{
+
+    public const string InvalidCredentials = " Invalid credentials.";
+    public const string NotFound = "Records Not found.";
+    public const string NotExist = "Not Exist.";
+    public const string IsRequired = "Required.";
+}
