d3c356acfcfe29cf8ccf2ac17b87999859025b7e
Osama Raed <osamaraed53@gmail.com>
2026-03-30 19:26:29 +0300

    add dependency injection file and use it method in program.cs file

---
+++ src/Backend/BrandInspector/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using BrandInspector.Data;
+using BrandInspector.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace BrandInspector;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+
+
+        return services;
+    }
+
+    public async static Task<WebApplication> UseServices(this WebApplication app)
+    {
+
+
+        return app;
+    }
+
+
+
+
+
+}
--- src/Backend/BrandInspector/Program.cs
+++ src/Backend/BrandInspector/Program.cs
@@ -6,13 +6,12 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddServices(builder.Configuration);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+await app.UseServices();
+
+
 
 app.UseHttpsRedirection();
 
