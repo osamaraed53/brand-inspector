273031c9edadd65efbd00b30cf0b7ff0e6a00a5a
Osama Raed <osamaraed53@gmail.com>
2026-03-30 21:38:27 +0300

    add and update error messages

--- src/Backend/BrandInspector/Constants/ErrorMessages.cs
+++ src/Backend/BrandInspector/Constants/ErrorMessages.cs
@@ -6,5 +6,7 @@ public static class ErrorMessages
     public const string InvalidCredentials = " Invalid credentials.";
     public const string NotFound = "Records Not found.";
     public const string NotExist = "Not Exist.";
-    public const string IsRequired = "Required.";
+    public const string IsRequired = "All fields required. Check your argument.";
+    public const string UserExist = "Username exists, please choose another one";
+
 }
