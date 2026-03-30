fb734caefb6d4835c19becd1ff25e3cc858e8656
Osama Raed <osamaraed53@gmail.com>
2026-03-30 19:45:52 +0300

    Add Swagger to the project and install require packages - swagger config in DI file -config of swagger contain bearer config

--- src/Backend/BrandInspector/DependencyInjection.cs
+++ src/Backend/BrandInspector/DependencyInjection.cs
@@ -1,7 +1,6 @@
 ﻿using BrandInspector.Data;
 using BrandInspector.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi;
 using Microsoft.OpenApi.Models;
 
 namespace BrandInspector;
@@ -17,17 +16,64 @@ public static class DependencyInjection
 
 
         services.AddScoped<IBrandConfigService, BrandConfigService>();
+
+
+        services.AddSwaggerConfig();
+
+
+
         return services;
     }
 
     public async static Task<WebApplication> UseServices(this WebApplication app)
     {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Brand Inspector Api");
+
+            });
+        }
 
 
         return app;
     }
 
 
+    private static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(opt =>
+        {
+            opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Brand Inspector", Version = "v1" });
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer"
+            });
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+        return services;
+
+    }
 
 
 
--- src/Backend/BrandInspector/Program.cs
+++ src/Backend/BrandInspector/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddServices(builder.Configuration);
