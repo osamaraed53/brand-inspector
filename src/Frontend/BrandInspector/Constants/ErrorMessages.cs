1ac62b446aeaa3a6b791bfd5f20abdc075af8871
Osama Raed <osamaraed53@gmail.com>
2026-04-01 10:45:26 +0300

    Update Error Messages - add new error message

--- src/Frontend/BrandInspector/Constants/ErrorMessages.cs
+++ src/Frontend/BrandInspector/Constants/ErrorMessages.cs
@@ -2,9 +2,10 @@
 {
     public  static class ErrorMessages
     {
-        public const string InvalidFile = "Invalid File.";        
+        public const string InvalidFile = "Invalid File, please choose another one.";        
         public const string InvalidCredentials = "Invalid credentials.";
         public const string InvalidFileType = "Only .pptx files are allowed.";
+        public const string UnSelectedFile = "Please select .pptx files before.";
 
     }
 }
